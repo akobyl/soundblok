f26a858c90ea281dbec9cc826fe7299f1aa87779
Andy Kobyljanec <kobyl125@gmail.com>
2023-07-24 20:48:43 -0400

    Updates to FreeRTOS module and build settings

--- src/main.c
+++ src/main.c
@@ -1,19 +1,27 @@
+#include "FreeRTOS.h"
+#include "queue.h"
 #include "wavemaker.h"
 
 int main(void) {
-    int x = 0;
-    while(x == 0);
+  int x = 0;
 
-    waveProperties_t test_wave = {
-        .amplitude = 10,
-        .center = 100,
-        .frequency = 100,
-        .type = WAVE_SAWTOOTH,
-        .sample_rate = 10000,
-    };
+  // QueueHandle_t queue = xQueueCreate(10, sizeof(uint32_t));
+  int y = configCPU_CLOCK_HZ;
+  while (x == 0)
+    ;
 
-    uint32_t buffer[100];
-    uint32_t wave_length = 0;
+  waveProperties_t test_wave = {
+      .amplitude = 10,
+      .center = 100,
+      .frequency = y,
+      .type = WAVE_SAWTOOTH,
+      .sample_rate = 10000,
+  };
 
-    generateSawtoothWave(buffer, 100, &test_wave, &wave_length);
+  uint32_t buffer[100];
+  uint32_t wave_length = 0;
+
+  generateSawtoothWave(buffer, 100, &test_wave, &wave_length);
 }
+
+void vApplicationMallocFailedHook() {}
